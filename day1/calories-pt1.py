ab396a7be5e18a9e899a2ddeae0d8a6fcf6f4362
Xandervdb1 <xandervandenbossche0@gmail.com>
2022-12-01 14:27:34 +0100

    refactor

--- day1/calories-pt1.py
+++ day1/calories-pt1.py
@@ -1,30 +1,20 @@
 datafile = open(r"C:\Users\xande\Dropbox\PC\Desktop\adventofcode22\day1\data.txt","r")
 data = datafile.readlines()
-
 sepList = []
 tempElf = []
 calories = []
-
 for x in data:
-    if x != "\n": # not a new tempElf
-        if x.endswith("\n"): #check if last value
-            l = len(x) 
-            numberStr = x[:l-1] #cut off "\n"
+    if x != "\n": #not a new tempElf
+        if x.endswith("\n"): #check if not last value
+            numberStr = x[:len(x)-1] #cut off "\n"
             number = int(numberStr) #change string to number
         else:
-            number = int(x) #not last value, so no cutting off
+            number = int(x) #last value, so no cutting off
         tempElf.append(number) #use temp list to store current elf
     else: #encountered "\n" which means a new elf is next
-        copyOfElf = tempElf.copy() #copy because lists reference each other
-        sepList.append(copyOfElf) #put current tempElf as a list inside the separated list
+        sepList.append(tempElf.copy()) #put current tempElf as a list inside the separated list
         tempElf.clear() #empty temp list
-copyOfElf = tempElf.copy() #make sure to include last value since no "\n" will be next
-sepList.append(copyOfElf)
-tempElf.clear()
-        
+sepList.append(tempElf.copy()) #make sure to include last value since no "\n" will be next
 for elf in sepList: #get the sum of calories for each list (elf) in separated list
-    calorie = sum(elf)
-    calories.append(calorie)
-
-max = max(calories) #get the max amount of calories from calories list
-print(max)
+    calories.append(sum(elf))
+print(max(calories)) #get the max amount of calories from calories list
--- day1/calories-pt2.py
+++ day1/calories-pt2.py
@@ -1,38 +1,26 @@
 datafile = open(r"C:\Users\xande\Dropbox\PC\Desktop\adventofcode22\day1\data.txt","r")
 data = datafile.readlines()
-
 sepList = []
 tempElf = []
 calories = []
 topThree = []
-
 for x in data:
-    if x != "\n": # not a new tempElf
-        if x.endswith("\n"): #check if last value
-            l = len(x) 
-            numberStr = x[:l-1] #cut off "\n"
+    if x != "\n": #not a new tempElf
+        if x.endswith("\n"): #check if not last value
+            numberStr = x[:len(x)-1] #cut off "\n"
             number = int(numberStr) #change string to number
         else:
-            number = int(x) #not last value, so no cutting off
+            number = int(x) #last value, so no cutting off
         tempElf.append(number) #use temp list to store current elf
     else: #encountered "\n" which means a new elf is next
-        copyOfElf = tempElf.copy() #copy because lists reference each other
-        sepList.append(copyOfElf) #put current tempElf as a list inside the separated list
+        sepList.append(tempElf.copy()) #put current tempElf as a list inside the separated list
         tempElf.clear() #empty temp list
-copyOfElf = tempElf.copy() #make sure to include last value since no "\n" will be next
-sepList.append(copyOfElf)
-tempElf.clear()
-        
+sepList.append(tempElf.copy()) #make sure to include last value since no "\n" will be next
 for elf in sepList: #get the sum of calories for each list (elf) in separated list
-    calorie = sum(elf)
-    calories.append(calorie) #make a list of the sum that each elf is carrying
-
+    calories.append(sum(elf))
 i = 1
 while i < 4: #need to find top 3, so repeat 3 times
-    maxCal = max(calories) #find biggest number in calories list
-    topThree.append(maxCal) #add it to top 3 list
-    index = calories.index(maxCal) #find the index of the biggest number
-    calories.pop(index) #remove biggest number from list
+    topThree.append(max(calories)) #add biggest num to top 3 list
+    calories.pop(calories.index(max(calories))) #remove biggest number from list
     i += 1
-
 print(sum(topThree))
